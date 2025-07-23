54d4bdda023c890579c54d963e77ebdf7f0560b0
Mariah <160214468+iahrchives@users.noreply.github.com>
2025-07-23 18:25:16 +0800

    Teller.java

--- BankTellerQueueSystem/src/main/java/com/bankteller/admin/teller/Teller.java
+++ BankTellerQueueSystem/src/main/java/com/bankteller/admin/teller/Teller.java
@@ -1,40 +1,18 @@
 package com.bankteller.admin.teller;
 
-import com.bankteller.admin.service.Service;
-
 public class Teller {
     private int id;
     private String name;
-    private Service serviceType;
-    private int transactionsHandled;
-    
-    public Teller() {
-        id = 1;
-        name = "Jane Doe";
-    }
+    private String serviceType;
 
-    public Teller(int id, String name) {
+    public Teller(int id, String name, String serviceType) {
         this.id = id;
         this.name = name;
         this.serviceType = serviceType;
-        this.transactionsHandled = transactionsHandled;
     }
 
     public int getId() { return id; }
     public String getName() { return name; }
-    public Service getServiceType() { return serviceType; }
-    public int getTransactionsHandled() { return transactionsHandled; }
-
-    public void setName(String name) { this.name = name; }
-    public void setServiceType(Service serviceType) { this.serviceType = serviceType; }
-    public void incrementTransactions() { transactionsHandled++; }
-
-    @Override
-    public String toString() {
-        return "ID: " + id +
-               " | Name: " + name +
-               " | Service: " + (serviceType != null ? serviceType.getName() : "Unassigned") +
-               " | Handled: " + transactionsHandled;
-    }
-
+    public String getServiceType() { return serviceType; }
 }
+
